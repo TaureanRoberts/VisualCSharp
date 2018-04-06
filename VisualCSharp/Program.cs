fd871ceb82b01d2c834a0070d84f5b318321cfa5
Taurean Roberts <taureanjroberts@gmail.com>
2018-04-06 15:55:36 -0500

    Restart computer

--- VisualCSharp/Program.cs
+++ VisualCSharp/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace VisualCSharp
 {
+    public class Vector2
+    {
+
+        public Vector2()
+        {
+        }
+        public Vector2(float lhs, float rhs)
+        {
+            float m_X = lhs;
+            float m_Y = rhs;
+        }
+        public static Vector2 operator
+        {
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
